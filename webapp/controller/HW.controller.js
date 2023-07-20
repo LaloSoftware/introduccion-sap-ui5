958d443aa006656bd7bacaef887e2e2398d4c9ba
eduardo lemus laguna <eduardo.lemus.laguna@gmail.com>
2023-07-20 14:07:12 -0600

    Integracion de algunos componentes de SAP

--- webapp/controller/HW.controller.js
+++ webapp/controller/HW.controller.js
@@ -1,10 +1,12 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/m/MessageBox"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, MessageToast, MessageBox) {
         "use strict";
 
         return Controller.extend("ns.proyecto1.controller.HW", {
@@ -22,6 +24,17 @@ sap.ui.define([
 
             onExit: function(){
                 console.log("Ciclo de vida terminado")
+            },
+
+            handlePressEvent: function(event){
+                MessageToast.show("Hola mundo");
+            },
+
+            handlePressBoxEvent: function(){
+                MessageBox.show("Hola mundo desde un box", {
+                    title: "Mi primer box",
+                    icon: MessageBox.Icon.SUCCESS
+                });
             },
         });
     });
